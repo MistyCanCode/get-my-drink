36ac2ca27884ba435812865b7ba13ccd9a0ae337
Misty Tomlin <mistycancode@gmail.com>
2022-04-09 16:02:03 -0500

    Updated to get a random index from the drinks array

--- js/main.js
+++ js/main.js
@@ -9,42 +9,56 @@ document.querySelector('#liquor').addEventListener('keypress', function(e){
 
 function getDrink(){
 let drink = document.querySelector('#liquor').value
+let index = 0
 
 const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
 
 fetch(url)
 .then(res => res.json()) //parse respnse as JSON
 .then(data => {
-    console.log(data.drinks[0])
-   document.querySelector('.name').innerText = data.drinks[0].strDrink
-   document.querySelector('img').src = data.drinks[0].strDrinkThumb
-   document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
-   document.querySelector('#one').innerText = data.drinks[0].strMeasure1 + " " + data.drinks[0].strIngredient1
-   document.querySelector('#two').innerText = data.drinks[0].strMeasure2 + " " + data.drinks[0].strIngredient2
-   document.querySelector('#three').innerText = data.drinks[0].strMeasure3 + " " + data.drinks[0].strIngredient3
-   if(data.drinks[0].strMeasure4 != null && data.drinks[0].strIngredient4 != null){
-     document.querySelector('#four').innerText = data.drinks[0].strMeasure4 + " " + data.drinks[0].strIngredient4
-     document.querySelector('#glass').innerText = data.drinks[0].strGlass
-   }else if(data.drinks[0].strMeasure4 != null && data.drinks[0].strIngredient4 === null){
-       document.querySelector('#four').innerText = data.drinks[0].strMeasure4
-       document.querySelector('#glass').innerText = data.drinks[0].strGlass
-   }else if(data.drinks[0].strIngredient4 != null && data.drinks[0].strMeasure4 === null){
-       document.querySelector("#four").innerText = data.drinks[0].strIngredient4
-       document.querySelector('#glass').innerText = data.drinks[0].strGlass
+    index = Math.ceil(Math.random() * data.length)
+    console.log(data.drinks[index])
+   document.querySelector('.name').innerText = data.drinks[index].strDrink
+   document.querySelector('img').src = data.drinks[index].strDrinkThumb
+   document.querySelector('#instructions').innerText = data.drinks[index].strInstructions
+   document.querySelector('#one').innerText = data.drinks[index].strMeasure1 + " " + data.drinks[index].strIngredient1
+   document.querySelector('#two').innerText = data.drinks[index].strMeasure2 + " " + data.drinks[index].strIngredient2
+   document.querySelector('#three').innerText = data.drinks[index].strMeasure3 + " " + data.drinks[index].strIngredient3
+   if(data.drinks[index].strMeasure4 != null && data.drinks[index].strIngredient4 != null){
+     document.querySelector('#four').innerText = data.drinks[index].strMeasure4 + " " + data.drinks[index].strIngredient4
+     document.querySelector('#glass').innerText = data.drinks[index].strGlass
+   }else if(data.drinks[index].strMeasure4 != null && data.drinks[index].strIngredient4 === null){
+       document.querySelector('#four').innerText = data.drinks[index].strMeasure4
+       document.querySelector('#glass').innerText = data.drinks[index].strGlass
+   }else if(data.drinks[index].strIngredient4 != null && data.drinks[index].strMeasure4 === null){
+       document.querySelector("#four").innerText = data.drinks[index].strIngredient4
+       document.querySelector('#glass').innerText = data.drinks[index].strGlass
    }else{
-       document.querySelector('#glass').innerText = data.drinks[0].strGlass
+       document.querySelector('#glass').innerText = data.drinks[index].strGlass
    }
-   if(data.drinks[0].strMeasure5 != null && data.drinks[0].strIngredient5 != null){
-    document.querySelector('#five').innerText = data.drinks[0].strMeasure5 + " " + data.drinks[0].strIngredient5
-    document.querySelector('#glass').innerText = data.drinks[0].strGlass
-  }else if(data.drinks[0].strMeasure5 != null && data.drinks[0].strIngredient5 === null){
-      document.querySelector('#five').innerText = data.drinks[0].strMeasure5
-      document.querySelector('#glass').innerText = data.drinks[0].strGlass
-  }else if(data.drinks[0].strIngredient5 != null && data.drinks[0].strMeasure5 === null){
-      document.querySelector("#five").innerText = data.drinks[0].strIngredient5
-      document.querySelector('#glass').innerText = data.drinks[0].strGlass
+   if(data.drinks[index].strMeasure5 != null && data.drinks[index].strIngredient5 != null){
+    document.querySelector('#five').innerText = data.drinks[index].strMeasure5 + " " + data.drinks[index].strIngredient5
+    document.querySelector('#glass').innerText = data.drinks[index].strGlass
+  }else if(data.drinks[index].strMeasure5 != null && data.drinks[index].strIngredient5 === null){
+      document.querySelector('#five').innerText = data.drinks[index].strMeasure5
+      document.querySelector('#glass').innerText = data.drinks[index].strGlass
+  }else if(data.drinks[index].strIngredient5 != null && data.drinks[index].strMeasure5 === null){
+      document.querySelector("#five").innerText = data.drinks[index].strIngredient5
+      document.querySelector('#glass').innerText = data.drinks[index].strGlass
   }else{
-      document.querySelector('#glass').innerText = data.drinks[0].strGlass
+      document.querySelector('#glass').innerText = data.drinks[index].strGlass
+  }
+     if(data.drinks[index].strMeasure6 != null && data.drinks[index].strIngredient6 != null){
+    document.querySelector('#six').innerText = data.drinks[index].strMeasure6 + " " + data.drinks[index].strIngredient6
+    document.querySelector('#glass').innerText = data.drinks[index].strGlass
+  }else if(data.drinks[index].strMeasure6 != null && data.drinks[index].strIngredient6 === null){
+      document.querySelector('#six').innerText = data.drinks[index].strMeasure6
+      document.querySelector('#glass').innerText = data.drinks[index].strGlass
+  }else if(data.drinks[index].strIngredient6 != null && data.drinks[index].strMeasure6 === null){
+      document.querySelector("#six").innerText = data.drinks[index].strIngredient6
+      document.querySelector('#glass').innerText = data.drinks[index].strGlass
+  }else{
+      document.querySelector('#glass').innerText = data.drinks[index].strGlass
   }
 })
 .catch(err => {
